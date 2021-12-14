ee3dddc1f830cc621d48dcd1464b0f36e86a9257
LR2711 <leandro.rodriguez@correounivalle.edu.co>
2021-12-13 19:26:07 -0500

    Cambio

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -307,8 +307,8 @@ class UserController extends Controller
         // }
         // return json_encode($user);
         $user = User::find($user->id);
-        $routine = Routine::where('routines.user_id', $user)->get();
-        return $routine;
+        $routine = Routine::where('routines.user_id', $user->id)->get();
+        return json_encode($routine);
     }
 
 }
