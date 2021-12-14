547e0471bd6a6ef56f96e1dd231da39a7a8278a2
LR2711 <leandro.rodriguez@correounivalle.edu.co>
2021-12-14 13:21:53 -0500

    Cambio

--- app/Http/Controllers/RoutineController.php
+++ app/Http/Controllers/RoutineController.php
@@ -3,24 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Routine;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
 
-class RoutinesController extends Controller
+class RoutineController extends Controller
 {
-    
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $variableJson = null;
-        
+
         $validator = Validator::make($request->all(), [
             'start_date' => 'required',
             'end_date' => 'required',
@@ -53,38 +44,5 @@ class RoutinesController extends Controller
                 ]);
             }
         }
-
-        return $variableJson;
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    // public function show($id)
-    // {
-    //     //
-    // }
-    public function showRoutine(Routine $routine)
-    {
-        $routine = Routine::find($routine->id);
-        $routine->excercises;
-        return json_encode($routine);
-    }
-    public function showUser(Routine $routine)
-    {
-        $routine = Routine::find($routine->id)->user;
-        $routine->user;
-        return json_encode($routine);
-    }
-    public function showUserRoutineB(Routine $routine)
-    {
-        // $routine = User::where(['user_id' => $user])->get();
-        $routine = Routine::find($routine);
-        return $routine;
-    }
-
-    
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
 /*
